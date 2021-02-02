1d0a9cbfdf0ee78a8ad1e437a91baf38f139fff5
smita patil <smitakpatil26@gmail.com>
2021-02-02 22:57:47 +0530

    updated some changes in code

--- Day2_Trim_Binary_Tree.java
+++ Day2_Trim_Binary_Tree.java
@@ -37,11 +37,11 @@ class Solution {
         }
        if(root.val < low)
        {
-           trimBST(root.right,low,high);
+           return trimBST(root.right,low,high);
        }
       if(root.val > high)
       {
-          trimBST(root.left,low,high);
+          return trimBST(root.left,low,high);
       }
         
         root.left=trimBST(root.left,low,high);
